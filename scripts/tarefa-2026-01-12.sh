4b056c86001b4e7257e65d4cd63cc2351d009d9d
Ricardo Rocha <rcarocha@gmail.com>
2026-01-08 17:41:29 -0300

    acrescimo de pip, virtualenv e spyder

--- scripts/tarefa-2026-01-12.sh
+++ scripts/tarefa-2026-01-12.sh
@@ -6,7 +6,10 @@ nos Laboratórios de Ensino 1 e 2
 
 Ref: Issue #11 em https://github.com/rcarocha/laboratorio-ufcat/issues/11
 
-Serão instalados (caso inexistentes) os pacotes: prolog, haskell, java-jdk, netbeans
+Serão instalados (caso inexistentes) os pacotes: 
+   prolog, haskell, java-jdk, netbeans
+   gh (CLI github), python3-pip python3-virtualenv spyder
+   
 IMPORTANTE: requer a indicação da senha do superusuário para execução do sudo
 
 EOF
@@ -14,4 +17,6 @@ read -p "Digite qualquer tecla quando estiver pronto. "
 
 sudo apt update
 sudo snap install netbeans --classic
-sudo apt -y install swi-prolog ghc default-jdk gh
+sudo apt -y install swi-prolog ghc default-jdk gh python3-pip python3-virtualenv spyder
+
+
