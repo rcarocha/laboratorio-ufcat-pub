3b31049e68dc1a15a83b1b14e7861f9cdf3a8a5f
Ricardo Rocha <rcarocha@gmail.com>
2026-05-24 09:59:04 -0300

    depencias BD e ajuste papel parede

---
+++ scripts/dependencias-2026-05-22.sh
@@ -0,0 +1,27 @@
+#!/bin/sh
+
+cat << EOF
+Os comandos a seguir instalarão as dependencias requeridas para a disciplina de
+Banco de Dados nos laboratórios de ensino.
+
+Ref: Issue #48 em https://github.com/rcarocha/laboratorio-ufcat/issues
+EOF
+
+read -p "Digite qualquer tecla quando estiver pronto. Requer indicacao da senha sudo"
+
+mkdir -p /tmp/tarefas
+cd /tmp/tarefas
+
+wget https://github.com/rcarocha/laboratorio-ufcat-pub/tree/main/figuras/papel-parede-oficial.webp
+cp papel-parede-oficial.webp /usr/share/lubuntu/wallpapers/papel-parede-oficial.webp
+chmod u-w /usr/share/lubuntu/wallpapers/papel-parede-oficial.webp
+
+wget https://github.com/rcarocha/laboratorio-ufcat-pub/tree/main/conf/dot.config/pcmanfm-qt/lxqt/settings.conf
+cp ~/.config/pcmanfm-qt/lxqt/settings.conf ~/.config/pcmanfm-qt/lxqt/settings.conf.bak
+cp settings.conf ~/.config/pcmanfm-qt/lxqt/
+chmod 444 ~/.config/pcmanfm-qt/lxqt/settings.conf
+
+sudo apt install -y kvm-ok
+sudo apt install -y umbrello
+sudo snap install dbeaver-ce --classic
+
